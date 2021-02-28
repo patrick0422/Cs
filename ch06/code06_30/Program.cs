1aae0d495f63f20e6061472291340ac0faab91c4
양태웅 <65935582+patrick0422@users.noreply.github.com>
2021-02-28 15:00:54 +0900

    6장 메서드

---
+++ ch06/code06_30/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace code06_30
+{
+    class Test
+    {
+        public int value = 10;
+    }
+    class Program
+    {
+        static void Change(Test input)
+        {
+            input.value = 20;
+        }
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.value = 10;
+            Change(test);
+
+            Console.WriteLine(test.value);
+        }
+    }
+}
