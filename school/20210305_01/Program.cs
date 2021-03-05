ba375b18b43da485e98f37d3af816317965e839b
patrick0422 <didxodnd0422@gmail.com>
2021-03-05 11:40:47 +0900

    응프

---
+++ school/20210305_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _20210305_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            WriteLine(a);
+
+            a += 2;
+
+            WriteLine(a);
+        }
+    }
+}
