6dff1f2cda8870fcb25177911983ee5e18c5010d
patrick0422 <didxodnd0422@gmail.com>
2021-03-11 11:18:12 +0900

    오늘의 응프

---
+++ school/20210311_01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210311_01
+{
+    class Program
+    {
+        enum Choices : int 
+        { 
+            SCISSOR,
+            ROCK,
+            PAPER
+        }
+
+        int choice, randChoice;
+        static void Print(int choice, int randChoice, string result)
+        {
+            string[] list = { "가위", "바위", "보" };
+            Console.WriteLine($"{list[choice]} : {list[randChoice]} - {result}");
+        }
+        static void Main()
+        {
+
+            Console.Write("입력>> ");
+
+            try
+            {
+                int choice = Int32.Parse(Console.ReadLine());
+                int randChoice = new Random().Next(0, 3);
+
+                if (choice == randChoice)
+                    Print(choice, randChoice, "비김");
+                else
+                {
+                    switch (choice)
+                    {
+                        case (int)Choices.SCISSOR:
+                            if (randChoice == (int)Choices.ROCK)
+                                Print(choice, randChoice, "짐");
+                            else if (randChoice == (int)Choices.PAPER)
+                                Print(choice, randChoice, "이김");
+                            break;
+                        case (int)Choices.ROCK:
+                            if (randChoice == (int)Choices.PAPER)
+                                Print(choice, randChoice, "짐");
+                            else if (randChoice == (int)Choices.SCISSOR)
+                                Print(choice, randChoice, "이김");
+                            break;
+                        case (int)Choices.PAPER:
+                            if (randChoice == (int)Choices.SCISSOR)
+                                Print(choice, randChoice, "짐");
+                            else if (randChoice == (int)Choices.ROCK)
+                                Print(choice, randChoice, "이김");
+                            break;
+                        default:
+                            Console.WriteLine("쉽지 않음");
+                            break;
+                    }
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("쉽지 않은 입력값");
+            }
+        }
+    }
+}
---
+++ school/20210311_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210311_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요 >> ");
+
+
+            int num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("무야호~");
+                    return;
+                }
+            }
+            Console.WriteLine(num + "은 소수입니다.");
+
+            return;
+        }
+    }
+}
