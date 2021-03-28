af0caa3a5818de4c93dd1979ead440e3084142f0
양태웅 <65935582+patrick0422@users.noreply.github.com>
2021-03-28 15:59:57 +0900

    고오급 폼

---
+++ weekend/Form/ch10/ex10.6_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex10._6_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
